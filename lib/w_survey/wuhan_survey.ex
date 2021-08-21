ae7c58f25b94eb9e8ae351a7f8f92fded5f796c4
karangejo <gabriel.g.gras@gmail.com>
2021-08-21 11:53:54 +0800

    refactor wuhan survey

--- lib/w_survey/wuhan_survey.ex
+++ lib/w_survey/wuhan_survey.ex
@@ -9,36 +9,38 @@ defmodule WSurvey.Surveys.WuhanSurvey do
       |> where(question: "The Wuhan coronavirus was created by China.")
       |> Repo.one()
 
-    %{
-      agree: w_survey.agree,
-      disagree: w_survey.disagree,
-      total: w_survey.agree + w_survey.disagree
-    }
+    {:ok,
+     %{
+       agree: w_survey.agree,
+       disagree: w_survey.disagree,
+       total: w_survey.agree + w_survey.disagree
+     }}
   end
 
   def wuhan_vote(vote) when vote in ["agree", "disagree"] do
-    with {:ok, survey} <-
-           Repo.transaction(fn ->
-             w_survey =
-               Survey
-               |> where(question: "The Wuhan coronavirus was created by China.")
-               |> lock("FOR UPDATE")
-               |> Repo.one()
+    case Repo.transaction(fn ->
+           w_survey =
+             Survey
+             |> where(question: "The Wuhan coronavirus was created by China.")
+             |> lock("FOR UPDATE")
+             |> Repo.one()
 
-             changeset =
-               case vote do
-                 "agree" ->
-                   Survey.changeset(w_survey, %{agree: w_survey.agree + 1})
+           changeset =
+             case vote do
+               "agree" ->
+                 Survey.changeset(w_survey, %{agree: w_survey.agree + 1})
 
-                 "disagree" ->
-                   Survey.changeset(w_survey, %{disagree: w_survey.disagree + 1})
-               end
+               "disagree" ->
+                 Survey.changeset(w_survey, %{disagree: w_survey.disagree + 1})
+             end
 
-             Repo.update(changeset)
-           end) do
-      survey
-    else
-      _ -> {:error, "Database error"}
+           Repo.update(changeset)
+         end) do
+      {:ok, survey} ->
+        survey
+
+      _ ->
+        {:error, "Database error"}
     end
   end
 
