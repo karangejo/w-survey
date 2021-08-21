afeec5e1a0800ce82bbb7da655b3cd8552ef5dee
karangejo <gabriel.g.gras@gmail.com>
2021-08-21 11:55:34 +0800

    clean up test rate limiter file

--- test/w_survey_web/rate_limiter_test.exs
+++ test/w_survey_web/rate_limiter_test.exs
@@ -1,11 +1,8 @@
 defmodule WSurvey.RateLimiterTest do
   use WSurveyWeb.ConnCase, async: true
 
-  alias WSurveyWeb.RateLimiter
   alias WSurvey.Surveys
 
-  @path "/api/count"
-  @rate_limit_options [max_requests: 1, interval_seconds: 60]
   @wuhan_survey_attrs %{question: "The Wuhan coronavirus was created by China."}
 
   def fixture(:w_survey) do
@@ -31,15 +28,15 @@ defmodule WSurvey.RateLimiterTest do
 
   test "displays error on rate limit", %{conn: conn} do
     fixture(:w_survey)
-    res_list = for _ <- 1..61 do
-      get(conn, Routes.wuhan_survey_path(conn, :wuhan_count))
+    for i <- 1..61 do
+      if i <= 60 do
+        conn = get(conn, Routes.wuhan_survey_path(conn, :wuhan_count))
+        assert json_response(conn, 200) == %{"count" => %{"agree" => 0, "disagree" => 0, "total" => 0}}
+      else
+        conn = get(conn, Routes.wuhan_survey_path(conn, :wuhan_count))
+        assert json_response(conn, 503) == %{"errors" => %{"detail" => "Service Unavailable"}}
+      end
     end
-    last_conn =
-      res_list
-      |> Enum.reverse()
-      |> List.first()
-
-    assert json_response(last_conn, 503) == %{"errors" => %{"detail" => "Service Unavailable"}}
   end
 
 end
